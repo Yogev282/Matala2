bc76b9ed09ab5887ff8ae7bc47ee1dc546afe61c
Yogev282 <yogev282000@gmail.com>
2022-12-03 13:53:19 +0200

    lastttttt

--- my_mat.c
+++ my_mat.c
@@ -3,33 +3,52 @@
 
 
 void floydWarshall(int mat[V][V]){ // algorithm for shortest way
-    int a, b, minsum , sum;
+    int a, b, sum;
 
     for(int k = 0 ; k < V ; k++){
         for(int i = 0 ; i < V ; i++){
             for(int j = 0 ; j < V ; j++){
                 a = mat[i][k];
                 b = mat[k][j];
-                sum = a+b;
-                if(a == 0 || b == 0){
+                if(a != 0 && b != 0){
+                    sum = a+b;
+                }
+                else {
                     sum = 0;
                 }
-                if( mat[i][j] == 0){
-                    minsum = sum;
+                if(i != j){
+                    if(sum == 0){
+                        //do nothing
+                    }
+                    else if(mat[i][j] == 0){
+                        mat[i][j] = sum;
+                    }
+                    else if(sum < mat[i][j]){
+                        mat[i][j] = sum;
+                    }
                 }
-                else if( sum == 0){
-                        minsum = mat[i][j];
-                }
-                else if( mat[i][j] < sum){
-                        minsum = mat[i][j];
-                }
-                else{
-                    minsum = sum;
-                }
-                if(i == j){ 
-                    minsum = 0;
-                }
-                mat[i][j] = minsum;
+            
+                
+                // sum = a+b;
+                // if(a == 0 || b == 0){
+                //     sum = 0;
+                // }
+                // if( mat[i][j] == 0){
+                //     minsum = sum;
+                // }
+                // else if( sum == 0){
+                //         minsum = mat[i][j];
+                // }
+                // else if( mat[i][j] < sum){
+                //         minsum = mat[i][j];
+                // }
+                // else{
+                //     minsum = sum;
+                // }
+                // if(i == j && mat[j] ==){ 
+                //     minsum = 0;
+                // }
+                // mat[i][j] = minsum;
             }
         }
     }
@@ -45,6 +64,7 @@ void matrixInsert(int mat[V][V]){ //insert the numbers to the matrix
             mat[i][j] = num;
         }
     }
+
     floydWarshall(mat); 
 }
 
@@ -59,10 +79,16 @@ void isRoutable(int mat[V][V] , int i , int j){ // checks if there is a way from
 }
 
 void shortest(int mat[V][V] , int i , int j){ // checks the shortest way from A to B depends on the algorithm
-     if(mat[i][j] == 0){
-        printf("-1\n");
+
+    if(i == j && mat[i][j] == 1){
+        printf("0\n");
     }
     else{
+        if(mat[i][j] == 0){
+        printf("-1\n");
+        }
+        else{
         printf("%d\n" , mat[i][j]);
+        }
     }
 }
